d987cef6ccaa021f324da5dc587f3fde30b4ab97
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-13 11:35:13 +0530

    5/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,11 +1,20 @@
 # Write your code here
 import random
-print("H A N G M A N")
+
+print("H A N G M A N\n")
 words = ['python', 'java', 'kotlin', 'javascript']
 word = random.choice(words)
-wordh = word[:3] + "-" * len(word[3:])
-guess = input("Guess the word {}: ".format(wordh))
-if guess == word:
-    print("You survived!")
-else:
-    print("You lost!")
+wordh = "-" * len(word)
+
+moves = 8
+while moves:
+    print(wordh)
+    guess = input("Input a letter: ")
+    if guess in word:
+        wordh = "".join([guess if word[i] == guess else wordh[i] for i in range(len(word))])
+    else:
+        print("That letter doesn't appear in the word")
+    moves -= 1
+    print()
+
+print("Thanks for playing!\nWe'll see how well you did in the next stage")
